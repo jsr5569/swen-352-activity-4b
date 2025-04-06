0ae08a9bf63ee53a9a1342c4ab0f1012954704cc
Domenic Mangano <domenicmangano@Domenics-MacBook-Pro.local>
2025-04-06 19:21:51 -0400

    pulling in updates

--- src/test/java/edu/rit/swen253/page/classSearch/ClassSearchPage.java
+++ src/test/java/edu/rit/swen253/page/classSearch/ClassSearchPage.java
@@ -1,4 +1,26 @@
 package edu.rit.swen253.page.classSearch;
 
-public class ClassSearchPage {
+import edu.rit.swen253.page.AbstractPage;
+import edu.rit.swen253.utils.DomElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import java.util.List;
+
+public class ClassSearchPage extends AbstractPage {
+    private static final By SELECT_TERM = By.className("bigTermPicker");
+    private static final By SELECTED_TERM = By.tagName("option");
+
+    public ClassSearchPage() { super(); }
+
+    public void selectSemesterTerm() {
+        DomElement selectTerm = findOnPage(SELECT_TERM);
+        selectTerm.sendKeys(Keys.ENTER);
+
+        List<DomElement> terms = findAllOnPage(SELECTED_TERM);
+        if (!terms.isEmpty()) {
+            DomElement fallSemester = terms.get(2);
+            fallSemester.sendKeys(Keys.ENTER);
+        }
+    }
 }
--- src/test/java/edu/rit/swen253/test/classSearch/SimpleClassSearchTest.java
+++ src/test/java/edu/rit/swen253/test/classSearch/SimpleClassSearchTest.java
@@ -1,4 +1,28 @@
 package edu.rit.swen253.test.classSearch;
 
+import edu.rit.swen253.page.classSearch.ClassSearchPage;
+import edu.rit.swen253.page.tiger.TigerCenterHomePage;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.*;
+
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.logging.Logger;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
 public class SimpleClassSearchTest {
+
+    public static Logger LOGGER = Logger.getLogger(SimpleClassSearchTest.class.getName());
+
+    private TigerCenterHomePage homePage;
+
+    private ClassSearchPage classSearchPage;
+
+    @Test
+    @Order(1)
+    @DisplayName("Test Navigate to Search Page")
+    void navigateToClassSearch() {
+        classSearchPage = navigateToClassSearch();
+    }
 }
