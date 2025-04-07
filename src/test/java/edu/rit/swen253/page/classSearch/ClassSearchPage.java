07a635837d1f071bc4823b8a7b411bc92f46e5be
Domenic Mangano <domenicmangano@Domenics-MacBook-Pro.local>
2025-04-06 20:16:32 -0400

    Simple Search test complete

--- src/test/java/edu/rit/swen253/page/classSearch/ClassSearchPage.java
+++ src/test/java/edu/rit/swen253/page/classSearch/ClassSearchPage.java
@@ -4,23 +4,47 @@ import edu.rit.swen253.page.AbstractPage;
 import edu.rit.swen253.utils.DomElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClassSearchPage extends AbstractPage {
-    private static final By SELECT_TERM = By.className("bigTermPicker");
-    private static final By SELECTED_TERM = By.tagName("option");
+    private static final By SELECT_TERM = By.tagName("select");
+    private static final By SEARCH_BAR = By.tagName("input");
+    private static final By SEARCH_RESULTS = By.className("classSearchResultsDisplayName");
+    private List<DomElement> searchResults;
+
 
     public ClassSearchPage() { super(); }
 
-    public void selectSemesterTerm() {
+    public Select selectSemesterTerm() {
         DomElement selectTerm = findOnPage(SELECT_TERM);
-        selectTerm.sendKeys(Keys.ENTER);
+        Select termDropdown = new Select(selectTerm.getWebElement());
+        termDropdown.selectByIndex(3);
+        return termDropdown;
+    }
 
-        List<DomElement> terms = findAllOnPage(SELECTED_TERM);
-        if (!terms.isEmpty()) {
-            DomElement fallSemester = terms.get(2);
-            fallSemester.sendKeys(Keys.ENTER);
+    public void searchForClass(String query) {
+        DomElement input = findOnPage(SEARCH_BAR);
+        input.sendKeys(query);
+        input.sendKeys(Keys.ENTER);
+    }
+
+    public List<String> getSearchResults() {
+        searchResults = findAllOnPage(SEARCH_RESULTS);
+        List<String> list = new ArrayList<>();
+        for (DomElement element : searchResults) {
+            String className = element.getText();
+            if (className != null) {
+                list.add(className);
+            }
         }
+        return list;
+    }
+
+    public String getFirstResult() {
+        List<String> list = getSearchResults();
+        return list.getFirst();
     }
 }
--- src/test/java/edu/rit/swen253/test/classSearch/SimpleClassSearchTest.java
+++ src/test/java/edu/rit/swen253/test/classSearch/SimpleClassSearchTest.java
@@ -2,27 +2,60 @@ package edu.rit.swen253.test.classSearch;
 
 import edu.rit.swen253.page.classSearch.ClassSearchPage;
 import edu.rit.swen253.page.tiger.TigerCenterHomePage;
+import edu.rit.swen253.test.AbstractWebTest;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.*;
 
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
 import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 
-public class SimpleClassSearchTest {
+public class SimpleClassSearchTest extends AbstractWebTest {
 
     public static Logger LOGGER = Logger.getLogger(SimpleClassSearchTest.class.getName());
 
-    private TigerCenterHomePage homePage;
+    private TigerCenterHomePage homePage ;
 
     private ClassSearchPage classSearchPage;
 
     @Test
     @Order(1)
     @DisplayName("Test Navigate to Search Page")
-    void navigateToClassSearch() {
-        classSearchPage = navigateToClassSearch();
+    void navigateToClassSearchTest() {
+        homePage = navigateToPage("https://tigercenter.rit.edu/tigerCenterApp/landing", TigerCenterHomePage::new);
+        homePage.selectClassSearch();
+        classSearchPage = new ClassSearchPage();
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Test Select Semester Term")
+    void selectSemesterTermTest() {
+        Select selectTerm = classSearchPage.selectSemesterTerm();
+        assertEquals("2025-26 Fall (2251)", selectTerm.getFirstSelectedOption().getText());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Test Enter Search Term")
+    void enterSearchTerm() {
+        classSearchPage.searchForClass("SWEN");
+        List<String> results = classSearchPage.getSearchResults();
+        assertNotEquals(results.size(), 0);
+        results.forEach(LOGGER::info);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Test Correct First Result")
+    void checkFirstResultTest() {
+        String result = classSearchPage.getFirstResult();
+        assertEquals("Independent Study", result);
     }
 }
